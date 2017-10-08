6eceba5e79d45478892fe80ce755dba0fa3758d5
Pradumn <pradumn.26@gmail.com>
2017-10-08 16:22:25 +0530

    deploying api 2

--- server/db/mongoose-db.js
+++ server/db/mongoose-db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://pradumn.26:spearbyedge1@ds113915.mlab.com:13915/todo-app');
+mongoose.connect('mongodb://pradumn:pradumn@ds113915.mlab.com:13915/todo-app');
 
 exports = module.exports = {
     mongoose: mongoose
